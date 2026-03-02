dcef7bb9449d3df6c6c3f52b4a4d35d2116760c8
Samuelcode7 <samuellcode7@gmail.com>
2026-03-01 23:33:28 -0300

    Remove comments for cleaner code
    Removed comments to clean up the code.

--- Main.java
+++ Main.java
@@ -2,11 +2,9 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        // Criando o repositório (array) de 10 voadores
         Voador[] listaDeVoadores = new Voador[10];
         Random gerador = new Random();
 
-        // 1. Sorteio e preenchimento da lista
         for (int i = 0; i < 10; i++) {
             int sorteio = gerador.nextInt(3);
             
@@ -19,11 +17,9 @@ public class Main {
             }
         }
 
-        // 2. Percorrendo a lista e executando as ações
         System.out.println("--- INICIANDO TESTE DE VOO ---");
         for (Voador v : listaDeVoadores) {
-            
-            // ABAIXO OCORREM AS CHAMADAS POLIMORFICAS (O Java decide o comportamento no runtime)
+
             v.voar();
             v.planar();
             v.pousar();

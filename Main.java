a1c7825842634367ab84b34226da61bf2533a76e
Samuelcode7 <samuellcode7@gmail.com>
2026-03-01 23:31:19 -0300

    Refactor flying object array and implement actions
    Refactor to use 'listaDeVoadores' for flying objects and implement polymorphic calls.

--- Main.java
+++ Main.java
@@ -2,18 +2,33 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        Voador[] lista = new Voador[10];
+        // Criando o repositório (array) de 10 voadores
+        Voador[] listaDeVoadores = new Voador[10];
         Random gerador = new Random();
 
+        // 1. Sorteio e preenchimento da lista
         for (int i = 0; i < 10; i++) {
-           
-            lista[i] = new Passaro("Sorteado " + i);
+            int sorteio = gerador.nextInt(3);
+            
+            if (sorteio == 0) {
+                listaDeVoadores[i] = new Passaro("Galinha Voadora");
+            } else if (sorteio == 1) {
+                listaDeVoadores[i] = new Passaro("Drone");
+            } else {
+                listaDeVoadores[i] = new Passaro("Passaro Sorteado " + i);
+            }
         }
 
-    
-        for (Voador v : lista) {
-            v.voar(); // 
-            System.out.println("---");
+        // 2. Percorrendo a lista e executando as ações
+        System.out.println("--- INICIANDO TESTE DE VOO ---");
+        for (Voador v : listaDeVoadores) {
+            
+            // ABAIXO OCORREM AS CHAMADAS POLIMORFICAS (O Java decide o comportamento no runtime)
+            v.voar();
+            v.planar();
+            v.pousar();
+            
+            System.out.println("-----------------------------");
         }
     }
 }
